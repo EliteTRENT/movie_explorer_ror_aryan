a1d2473b0083bc428b1791bcd510872b5433ccc5
Aryan Negi <negiaryan2803@gmail.com>
2025-05-01 11:18:46 +0530

    [Aryan] MOD: Fixed swagger implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  respond_to :json
+
+  protected
+
+  def authenticate_user!
+    unless current_user
+      render json: { error: 'You need to sign in first.' }, status: :unauthorized
+    else
+      super
+    end
+  end
 end
